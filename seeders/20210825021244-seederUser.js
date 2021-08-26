79f826001c9fd570b098906d8b23efd1374283f1
“[Krisna <[kdkrisnadwipayana@gmail.com]>
2021-08-26 21:32:05 +0800

    fixed seed

--- seeders/20210825021244-seederUser.js
+++ seeders/20210825021244-seederUser.js
@@ -1,24 +1,33 @@
 "use strict";
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-     */
-  },
+    up: (queryInterface, Sequelize) => {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
+        let data = [{
+            username: "mamaa",
+            email: "kaka@gmail.com",
+            password: "skasaksna",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }]
+        return queryInterface.bulkInsert("Users", data);
+    },
 
-  down: (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  },
+    down: (queryInterface, Sequelize) => {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        return queryInterface.bulkDelete("Users", null);
+    },
 };
